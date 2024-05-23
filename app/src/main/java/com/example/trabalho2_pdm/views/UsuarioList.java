d28cf276af264b21e71aeb7da039381b71a5885f
Edgar de ROsis <edgarrosis@gmail.com>
2024-05-22 23:24:06 -0400

    Cadastro de usuarios implementado, redirecionamento para tela de modificação bugada

--- app/src/main/java/com/example/trabalho2_pdm/views/UsuarioList.java
+++ app/src/main/java/com/example/trabalho2_pdm/views/UsuarioList.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +19,8 @@ import java.util.List;
 public class UsuarioList extends AppCompatActivity {
     private ActivityUsuarioListBinding binding;
     private LocalDatabase db;
+    private int dbUsuarioID;
+    private Usuario dbUsuario;
     private List<Usuario> usuarios;
     private ListView listViewUsuario;
     private Intent edtIntent;
@@ -37,30 +40,63 @@ public class UsuarioList extends AppCompatActivity {
             }
         });
         binding.addBttnUsuario.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(UsuarioList.this, UsuarioView.class));
+        @Override
+        public void onClick(View view) {
+            String nomeUsuario = binding.edtNomeUsuario.getText().toString();
+            if (nomeUsuario.isEmpty()) {
+                Toast.makeText(UsuarioList.this, "Preencha todos os Campos",
+                        Toast.LENGTH_SHORT).show();
+                return;
             }
-        });
-    }
+
+            String emailUsuario = binding.edtEmailUsuario.getText().toString();
+            if (emailUsuario.isEmpty()) {
+                Toast.makeText(UsuarioList.this, "Preencha todos os Campos",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            String senhaUsuario = binding.edtSenhaUsuario.getText().toString();
+            if (senhaUsuario.isEmpty()) {
+                Toast.makeText(UsuarioList.this, "Preencha todos os Campos",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Usuario thisUsuario = new Usuario(dbUsuarioID,nomeUsuario, emailUsuario, senhaUsuario);
+
+
+
+            if (dbUsuario != null) {
+                thisUsuario.setUsuarioID(dbUsuarioID);
+                thisUsuario.setNome(nomeUsuario);
+                thisUsuario.setEmail(emailUsuario);
+                thisUsuario.setSenha(senhaUsuario);
+
+                db.usuarioModel().update(thisUsuario);
+            } else {
+                db.usuarioModel().insertAll(thisUsuario);
+            }
+            onResume();
+        }
+    });
+}
     @Override
     protected void onResume() {
         super.onResume();
         edtIntent = new Intent(this, UsuarioView.class);
-        preencheUsuarios();
+        preencherUsuarios();
     }
-    private void preencheUsuarios() {
+    private void preencherUsuarios(){
         usuarios = db.usuarioModel().getAll();
-        ArrayAdapter<Usuario> usuariosAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, usuarios);
-        listViewUsuario.setAdapter(usuariosAdapter);
+        ArrayAdapter<Usuario> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                usuarios);
+        listViewUsuario.setAdapter(adapter);
 
         listViewUsuario.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Usuario usuarioSelecionado = usuarios.get(position);
-                edtIntent.putExtra("MARCA_SELECIONADA_ID",
-                        usuarioSelecionado.getUsuarioID());
+                edtIntent.putExtra("Usuario_Selecionado_ID", usuarioSelecionado.getUsuarioID());
                 startActivity(edtIntent);
             }
         });
