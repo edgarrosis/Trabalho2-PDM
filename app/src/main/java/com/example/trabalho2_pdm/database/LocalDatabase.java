3024266eaab0112796517a8f7aa7060f3d872011
Edgar de ROsis <edgarrosis@gmail.com>
2024-05-25 13:51:53 -0400

    cadastro,alteração e exclusao de cidades implementado. Cadastro de endereços implementado, faltando alteração e exclusao de endereços e adicionar a API de mapa

--- app/src/main/java/com/example/trabalho2_pdm/database/LocalDatabase.java
+++ app/src/main/java/com/example/trabalho2_pdm/database/LocalDatabase.java
@@ -12,14 +12,15 @@ import com.example.trabalho2_pdm.entities.Endereco;
 import com.example.trabalho2_pdm.entities.Usuario;
 import com.example.trabalho2_pdm.entities.Cidade;
 
-@Database(entities = {Usuario.class, Cidade.class, Endereco.class}, version = 3)
+@Database(entities = {Usuario.class, Cidade.class, Endereco.class}, version = 4)
 public abstract class LocalDatabase extends RoomDatabase {
     private static LocalDatabase INSTANCE;
 
     public static LocalDatabase getDatabase(Context context) {
         if(INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    LocalDatabase.class, "ControleUsuarios").allowMainThreadQueries().build();
+                    LocalDatabase.class, "ControleUsuarios").fallbackToDestructiveMigration()
+                    .allowMainThreadQueries().build();
         }
         return INSTANCE;
     }
